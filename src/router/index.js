5ffab81390688bc2b87108634ee50ba41a752128
zhaolin <zhaolin@yzw.cn>
2024-01-16 10:31:33 +0800

    feat: vue2测试页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,90 +1,93 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import store from '../store'
+import store from "../store";
 
-Vue.use(Router)
+Vue.use(Router);
 
-import ShopGoods from '../views/Shop/ShopGoods/ShopGoods.vue'
-import ShopInfo from '../views/Shop/ShopInfo/ShopInfo.vue'
-import ShopRatings from '../views/Shop/ShopRatings/ShopRatings.vue'
+import ShopGoods from "../views/Shop/ShopGoods/ShopGoods.vue";
+import ShopInfo from "../views/Shop/ShopInfo/ShopInfo.vue";
+import ShopRatings from "../views/Shop/ShopRatings/ShopRatings.vue";
 
 const routes = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home"
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/Login/login.vue')
+    path: "/test",
+    name: "test",
+    component: () => import("../views/Test/index.vue")
   },
   {
-    path: '/regist',
-    name: 'regist',
-    component: () => import('../views/Login/login.vue')
+    path: "/login",
+    name: "login",
+    component: () => import("../views/Login/login.vue")
   },
   {
-    path: '/home',
-    name: 'home',
-    component: () => import('../views/Home/home.vue')
+    path: "/regist",
+    name: "regist",
+    component: () => import("../views/Login/login.vue")
   },
   {
-    path: '/search',
-    name: 'search',
-    component: () => import('../views/Search/search.vue')
+    path: "/home",
+    name: "home",
+    component: () => import("../views/Home/home.vue")
   },
   {
-    path: '/order',
-    name: 'order',
-    component: () => import('../views/Order/order.vue')
+    path: "/search",
+    name: "search",
+    component: () => import("../views/Search/search.vue")
   },
   {
-    path: '/mine',
-    name: 'mine',
-    component: () => import('../views/Mine/mine.vue')
+    path: "/order",
+    name: "order",
+    component: () => import("../views/Order/order.vue")
   },
   {
-    path: '/shop',
-    name: 'shop',
-    component: () => import('../views/Shop/Shop.vue'),
+    path: "/mine",
+    name: "mine",
+    component: () => import("../views/Mine/mine.vue")
+  },
+  {
+    path: "/shop",
+    name: "shop",
+    component: () => import("../views/Shop/Shop.vue"),
     children: [
       {
-        path: '/shop/goods',
-        component: ShopGoods 
+        path: "/shop/goods",
+        component: ShopGoods
       },
       {
-        path: '/shop/ratings',
-        component: ShopRatings 
+        path: "/shop/ratings",
+        component: ShopRatings
       },
       {
-        path: '/shop/info',
+        path: "/shop/info",
         component: ShopInfo
-      },
+      }
     ]
-  },
-]
+  }
+];
 
 const router = new Router({
   routes,
   base: process.env.BASE_URL
-})
+});
 
 router.beforeEach((to, from, next) => {
-  if(to.meta.requireAuth) {
-    if(store.getters.token) {
-      next()
-    }
-    else {
+  if (to.meta.requireAuth) {
+    if (store.getters.token) {
+      next();
+    } else {
       next({
-        path: '/login',
-        query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
-      })
+        path: "/login",
+        query: { redirect: to.fullPath } // 将跳转的路由path作为参数，登录成功后跳转到该路由
+      });
     }
+  } else {
+    next();
   }
-  else {
-    next()
-  }
-})
+});
 
-export default router
+export default router;
